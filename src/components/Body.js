bf0beb0b8e75c177f29c6fb940d620f76d5b5862
khemchand7 <khemchandk.@gmail.com>
2024-07-28 21:48:07 +0530

    deleted watch trailer.

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,7 +2,6 @@ import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Login from "./Login";
 import Browse from "./Browse";
-import WatchTrailer from "./WatchTrailer";
 
 
 const Body = () => {
@@ -15,10 +14,6 @@ const Body = () => {
     {
       path: "browse/",
       element: <Browse />,
-    },
-    {
-      path: "/browse/watch/:id",
-      element: <WatchTrailer/> ,
     },
   ]);
 
